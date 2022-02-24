44d11a88199b79a9dcbcb7f41f65e65e627cbd53
crenaz <crenaz@gmail.com>
2022-02-24 00:12:52 -0400

    trying out Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use yew::prelude::*;
+
+#[function_componet]
+fn App() -> Html {
+    Let counter = use_state(|| 0);
+    Let onclick = {
+        Let counter = counter.clone();
+        move |_| {
+            Let value = *couter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1+" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::startapp::<App>();
 }
